4a3005f1232caf19f2fe3e4cd5b36b03de08a208
Juergen Hess <juergen.hess@hesspost.de>
2022-03-14 21:15:45 +0100

    Added subdirectories as dependencies

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import setup, find_packages
 setup(
     name='pendulum',
     version='0.1',
-    packages=['pendulum', 'pendulum.driver'],
+    packages=['pendulum', 'pendulum.driver', 'pendulum.envs'],
     url='https://github.com/juehess/pendulum',
     license='',
     author='Juergen Hess',
     author_email='juergen@hesspost.de',
     description='Inverted Pendulum Control',
-    python_requires=">=3"
+    python_requires=">=3",
+    install_requires=['gym','smbus2']
 )
